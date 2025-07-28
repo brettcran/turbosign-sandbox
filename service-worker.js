d9f5896fa0664818231816effd77c9bdb7e31737
brettcran <77210142+brettcran@users.noreply.github.com>
2025-07-28 14:06:58 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+
+const CACHE_NAME = 'turbosign-cache-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-256.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
